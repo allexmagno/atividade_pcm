ed64102bafb819a187674d80e9d0a9cd7a1dd5f4
Allex Magno Andrade <ct.allex@gmail.com>
2019-05-18 16:49:51 -0300

    Arrumando bugs

--- rtx_pcm.m
+++ rtx_pcm.m
@@ -52,50 +52,50 @@ x_bin = de2bi(x_qtz); % vai gerar uma matriz
 
 %% Processo de compressao
 i = 1;
-x_comp = [];
+x_c = [];
 while i <= x
-  j = 2;
+  cont = 2;
   flag = 1;
-  while j <= 8
-    if x_bin(i,j:j) == 1
+  while cont <= 8
+    if x_bin(i,cont:cont) == 1
       if flag == 1
-        cont = j;
-        flag = 1;
-      else
-        cont = 0;
-        flag = 0
+        j = cont;
+        flag = 0;
       end
     end
-    j = j+ 1;
+    cont = cont + 1;
   end
   
-  if j == 1
-    s = [1 1 1];
-    comp = [s x_bin(i,j+1:5)];
-  elseif j == 2
-    s = [1 1 0];
-    comp = [s x_bin(i,j+1:6)]; 
-  elseif j == 3
-    s = [1 0 1];
-    comp = [s x_bin(i,j+1:7)];
-  elseif j == 4
-    s = [1 0 0];
-    comp = [s x_bin(i,j+1:8)];
-  elseif j == 5
-    s = [0 1 1];
-    comp = [s x_bin(i,j+1:9)];
-  elseif j == 6
-    s = [0 1 0];
-    comp = [s x_bin(i,j+1:10)];
-  elseif j == 7
-    s = [0 0 1];
-    comp = [s x_bin(i,j+1:11)];
-  elseif j > 7
-    s = [0 0 0];
-    comp = [s x_bin(i,j+1:12)];
+  if flag == 0
+    if j == 2
+      s = [1 1 1];
+      comp = [x_bin(i,1:1) s x_bin(i,j+1:6)];
+    elseif j == 3
+      s = [1 1 0];
+      comp = [x_bin(i,1:1) s x_bin(i,j+1:7)]; 
+    elseif j == 4
+      s = [1 0 1];
+      comp = [x_bin(i,1:1) s x_bin(i,j+1:8)];
+    elseif j == 5
+      s = [1 0 0];
+      comp = [x_bin(i,1:1) s x_bin(i,j+1:9)];
+    elseif j == 6
+      s = [0 1 1];
+      comp = [x_bin(i,1:1) s x_bin(i,j+1:10)];
+    elseif j == 7
+      s = [0 1 0];
+      comp = [x_bin(i,1:1) s x_bin(i,j+1:11)];
+    elseif j == 8
+      s = [0 0 1];
+      comp = [x_bin(i,1:1) s x_bin(i,j+1:12)];
+    end
+    else
+      s = [0 0 0];
+      comp = [x_bin(i,1:1) s x_bin(i,9:12)];
   end
   i = i + 1;
-  x_comp = [x_comp comp];
+  x_c = [x_c; comp];
+
 end
 
 %x_bin_linha = reshape(transpose(x_bin), 1, x*y); % Para colocar tudo em uma unica linha 
@@ -108,17 +108,17 @@ subplot(313)
 filtro_NRZ = ones(1, 100); % filtro
 %tx = filter(filtro_NRZ, 1, x_bin_linha);
 %tx = filter(filtro_NRZ, 1, upsample(x_bin_linha, 100));
-tx = filter(filtro_NRZ, 1, upsample(x_comp, 100));
-plot(tx);
-ylim([-0.5 2])
-xlim([2.3e6 2.35e6])
-title('Transmissão do sinal')
+%tx = filter(filtro_NRZ, 1, upsample(x_comp, 100));
+%plot(tx);
+%ylim([-0.5 2])
+%xlim([2.3e6 2.35e6])
+%title('Transmissão do sinal')
 
 %% Recuperando o Sinal
 % rrr = reshape(xxx, coluna, linha)
 % reshape(rrr', linha, coluna)
-x_compressado = reshape(x_comp, 8, length(x_comp)/8);
-x_c = reshape(x_compressado', length(x_comp)/8, 8);
+%x_compressado = reshape(x_comp, 8, length(x_comp)/8);
+%x_c = reshape(x_compressado', length(x_comp)/8, 8);
 
 %% expansão
 i = 1;
@@ -152,13 +152,13 @@ while i <= x
     b = [x_c(i,1:1) 1 x_c(i,5:8) 1 0 0 0 0 0 0];
   end
   i = i + 1;
-  x_exp = [x_exp b];
+  x_exp = [x_exp; b];
 end
 
-x_exp_dec = reshape(x_exp, k, length(x_exp)/k);
-xdec_qtz = reshape(x_exp_dec', length(x_exp)/k, k); % sinal ainda quantizado
+%x_exp_dec = reshape(x_exp, k, length(x_exp)/k);
+%xdec_qtz = reshape(x_exp_dec', length(x_exp)/k, k); % sinal ainda quantizado
 %xdec_qtz1 = transpose(xdec_qtz); % o sinal original em uma coluna
-xdec = bi2de(xdec_qtz); % ou xdec = bi2de(transpose(xdec_qtz1)) 
+xdec = bi2de(x_exp); % ou xdec = bi2de(transpose(xdec_qtz1)) 
 xdec_passo = xdec * passo;
 xdec_rec = xdec_passo - ((Vpp/2) - (passo/2));
 f_PB = fir1(50, 10e3/Fs);
@@ -169,10 +169,10 @@ yy = conv(f_PB, xdec_rec);
 %% analizando o espctro
 X_f = fftshift(fft(x_n)/length(x_n));
 Xdec_f = fftshift(fft(yy)/length(yy));
-f = -Fs/2:1/(length(x_n)/Fs):Fs/2-(1/length(x_n)/Fs);%-1;
+f = -Fs/2:1/(length(Xdec_f)/Fs):Fs/2-(1/length(Xdec_f)/Fs);%-1;
 figure(2)
 subplot(211)
 plot(yy)
 subplot(212)
-plot(f, transpose(abs(Xdec_f(1:end-50))))
+plot(f, (abs(Xdec_f)))
 xlim([-10e3 10e3])
